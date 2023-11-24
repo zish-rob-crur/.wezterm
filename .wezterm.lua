a61a5a01e44d1cfa929dcbaa55ca08a563128fc6
zhiwen.wang <zhiwen.wang@airudder.com>
2023-11-24 15:58:57 +0800

    1. init .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require 'wezterm'
+
+return {
+    color_scheme_dirs = { "~/GitHubRepos/flexoki/wezterm" },
+    color_scheme = "Flexoki Dark",
+    window_decorations = "RESIZE",
+    font_size = 15.0,
+    tab_max_width = 16,
+    show_new_tab_button_in_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
+    keys = {{
+        key = "LeftArrow",
+        mods = "ALT",
+        action = wezterm.action {
+            SendKey = {
+                key = "LeftArrow",
+                mods = "CTRL"
+            }
+        }
+    }, {
+        key = "RightArrow",
+        mods = "ALT",
+        action = wezterm.action {
+            SendKey = {
+                key = "RightArrow",
+                mods = "CTRL"
+            }
+        }
+    }}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # .wezterm.lua
-wezterm config 
+
+## Installation
+
+```bash
+git clone git@github.com:zish-rob-crur/.wezterm.git
+git clone https://github.com/kepano/flexoki.git ~/GitHubRepos/flexoki
+ln -s .wezterm/.wezterm.lua .wezterm.lua 
+```
+
+enjoyment of the terminal
+
+## Screenshots
+
