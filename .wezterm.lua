f7ee4bc0174748b81ef4898055b13d506c3d3634
zhiwen.wang <zhiwen.wang@airudder.com>
2023-11-24 16:06:20 +0800

    1. 更新 tab max width

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,11 @@
 local wezterm = require 'wezterm'
 
 return {
-    color_scheme_dirs = { "~/GitHubRepos/flexoki/wezterm" },
+    color_scheme_dirs = {"~/GitHubRepos/flexoki/wezterm"},
     color_scheme = "Flexoki Dark",
     window_decorations = "RESIZE",
     font_size = 15.0,
-    tab_max_width = 16,
+    tab_max_width = 25,
     show_new_tab_button_in_tab_bar = false,
     hide_tab_bar_if_only_one_tab = true,
     keys = {{
